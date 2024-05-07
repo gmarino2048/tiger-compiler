19a25e6a870b9eb07b83989c0738a533b07760a4
Guy Marino <gmarino2048@gmail.com>
2024-05-07 19:08:42 -0400

    Refactor the quantifier parse code

--- regex/src/parse/quantifier.rs
+++ regex/src/parse/quantifier.rs
@@ -33,38 +33,29 @@ impl Quantifier {
     }
 
     fn parse_quantifier_range(input: &String) -> ParseResult<Quantifier> {
-        let components = input
+        let components = Self::split_range_components(input);
+        let range_elements = Self::collect_parse_errors(components)?;
+
+        if range_elements.len() == 2 {
+            Self::create_ranged_quantifier(range_elements[0], range_elements[1])
+        }
+        else if range_elements.len() == 1 {
+            Self::create_ranged_quantifier(None, range_elements[0])
+        }
+        else {
+            let range_field_count = range_elements.len();
+            let msg = format!("Cannot create a ranged quantifier with {range_field_count} fields.");
+            Err(Error::new(ErrorKind::InvalidQuantifier, msg.as_str()))
+        }
+    }
+
+    fn split_range_components(input: &String) -> Vec<ParseResult<Option<usize>>> {
+        input
             .split(',')
             .map(|s| s.replace(&['{', '}'], ""))
             .map(|s| String::from(s.trim()))
             .map(|s| Self::parse_integer_or_empty(s.as_str()))
-            .collect::<Vec<_>>();
-
-        let mut range = Vec::with_capacity(components.len());
-        for component in components {
-            let count = component?;
-            range.push(count);
-        }
-
-        if range.len() == 2 {
-            match (&range[0], &range[1]) {
-                (Some(start), None) => Ok(Quantifier::AtLeast(*start)),
-                (None, Some(finish)) => Ok(Quantifier::AtMost(*finish)),
-                (Some(start), Some(finish)) => Ok(Quantifier::Range(*start, *finish)),
-                _ => Err(Error::new(ErrorKind::InvalidQuantifier, "Cannot create ranged quantifier with two empty fields."))
-            }
-        }
-        else if range.len() == 1 {
-            match &range[0] {
-                Some(value) => Ok(Quantifier::AtLeast(*value)),
-                None => Err(Error::new(ErrorKind::InvalidQuantifier, "Cannot create a ranged quantifier with an empty field."))
-            }
-        }
-        else {
-            let range_field_count = range.len();
-            let msg = format!("Cannot create a ranged quantifier with {range_field_count} fields.");
-            Err(Error::new(ErrorKind::InvalidQuantifier, msg.as_str()))
-        }
+            .collect::<Vec<_>>()
     }
 
     fn parse_integer_or_empty(input: &str) -> ParseResult<Option<usize>> {
@@ -76,6 +67,26 @@ impl Quantifier {
                 Ok(parse_result) => Ok(Some(parse_result)),
                 Err(error) => Err(Error::new(ErrorKind::InvalidSyntax, error.to_string().as_str()))
             }
+        }
+    }
+
+    fn collect_parse_errors(results: Vec<ParseResult<Option<usize>>>) -> ParseResult<Vec<Option<usize>>> {
+        let mut range_elements = Vec::with_capacity(results.len());
+
+        for parse_result in results {
+            let component = parse_result?;
+            range_elements.push(component);
+        }
+
+        Ok(range_elements)
+    }
+
+    fn create_ranged_quantifier(start: Option<usize>, end: Option<usize>) -> ParseResult<Quantifier> {
+        match (start, end) {
+            (Some(start_value), None) => Ok(Quantifier::AtLeast(start_value)),
+            (None, Some(end_value)) => Ok(Quantifier::AtMost(end_value)),
+            (Some(start_value), Some(end_value)) => Ok(Quantifier::Range(start_value, end_value)),
+            _ => Err(Error::new(ErrorKind::InvalidQuantifier, "Cannot create ranged quantifier with two empty fields."))
         }
     }
 }
